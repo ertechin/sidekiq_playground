ed0a5d8f2487a3c68afc27fe4d50390061596a6c
ertechin <erselertekin@hotmail.com>
2024-08-27 01:54:29 +0300

    devise user & devise configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(email: 'admin@admin.com', password: '123456', password_confirmation: '123456', admin: true)
+User.create!(email: 'test@test.com', password: '123456', password_confirmation: '123456', admin: false)
